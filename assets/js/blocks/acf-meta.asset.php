e6ec86956350aeb40c32ac00be40326c037e006a
Spencer Blacker <spencerblacker@Samantaamba3377.localdomain>
2026-04-16 11:32:05 +0100

    fix: add .asset.php files so block scripts load after WP dependencies
    Without these files, WordPress enqueues the block editor JS before
    wp-blocks / wp-block-editor / wp-components / wp-element are ready,
    so registerBlockType never fires and blocks don't appear in the inserter.

---
+++ assets/js/blocks/acf-meta.asset.php
@@ -0,0 +1,1 @@
+<?php return array( "dependencies" => array( "wp-blocks", "wp-block-editor", "wp-components", "wp-element", "wp-server-side-render", "wp-data" ), "version" => "1.3.0" );
---
+++ assets/js/blocks/ds-accordion.asset.php
@@ -0,0 +1,1 @@
+<?php return array( "dependencies" => array( "wp-blocks", "wp-block-editor", "wp-components", "wp-element" ), "version" => "1.3.0" );
